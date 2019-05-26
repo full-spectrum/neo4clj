0ab513aff4ce53aaa1690a1b49c4e15846e07e7a
Jacob Emcken <jacob@emcken.dk>
2019-05-26 09:52:02 +0200

    Cleanup unused parts in project.clj
    This also fixed a bug causing `lein repl` to throw an error:
    
        Could not locate user__init.class, user.clj or user.cljc on classpath.

--- project.clj
+++ project.clj
@@ -5,9 +5,6 @@
                  [com.rpl/specter "1.1.2"]
                  [org.clojure/clojure "1.10.0"]
                  [org.neo4j.driver/neo4j-java-driver "1.7.2"]]
-  :main full-spectrum.neo4clj.client
-  :aot [full-spectrum.neo4clj.client]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [criterium "0.4.4"]]
-                   :source-paths ["dev"]
-                   :repl-options {:init-ns user}}})
+                   :source-paths ["dev"]}})
