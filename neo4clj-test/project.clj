054a5470b644352eaee1b987bfa92a45bc432c2a
Claus Engel-Christensen <cec@localhost.localdomain>
2022-11-29 23:14:42 +0100

    Release new minor version 1.0.1

--- neo4clj-test/project.clj
+++ neo4clj-test/project.clj
@@ -1,10 +1,10 @@
-(defproject com.github.full-spectrum/neo4clj-test "1.0.0"
+(defproject com.github.full-spectrum/neo4clj-test "1.0.1"
   :description "Neo4j test library"
   :url "https://github.com/full-spectrum/neo4clj"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [com.github.full-spectrum/neo4clj-core "1.0.0"]
+                 [com.github.full-spectrum/neo4clj-core "1.0.1"]
                  [org.neo4j.driver/neo4j-java-driver "4.4.9"]
                  [org.neo4j/neo4j-dbms "4.4.11"]
                  [org.neo4j/neo4j "4.4.11" :exclusions [org.slf4j/slf4j-api]]
