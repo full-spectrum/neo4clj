d168d3b2f1c4fc88b15cd22fd044bbdf181dc2da
Claus Engel-Christensen <cec@localhost.localdomain>
2023-01-24 21:16:38 +0100

    Update neo4clj-test to use Neo4j 5.3.0

--- neo4clj-test/project.clj
+++ neo4clj-test/project.clj
@@ -1,13 +1,13 @@
-(defproject com.github.full-spectrum/neo4clj-test "1.0.1"
+(defproject com.github.full-spectrum/neo4clj-test "1.1.0"
   :description "Neo4j test library"
   :url "https://github.com/full-spectrum/neo4clj"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [com.github.full-spectrum/neo4clj-core "1.0.1"]
-                 [org.neo4j.driver/neo4j-java-driver "4.4.9"]
-                 [org.neo4j/neo4j-dbms "4.4.11"]
-                 [org.neo4j/neo4j "4.4.11" :exclusions [org.slf4j/slf4j-api]]
-                 [org.neo4j/neo4j-graphdb-api "4.4.11"]
-                 [org.neo4j/neo4j-bolt "4.4.11" :exclusions [org.slf4j/slf4j-api]]]
+                 [com.github.full-spectrum/neo4clj-core "1.1.0"]
+                 [org.neo4j.driver/neo4j-java-driver "5.3.0"]
+                 [org.neo4j/neo4j-dbms "5.3.0"]
+                 [org.neo4j/neo4j "5.3.0" :exclusions [org.slf4j/slf4j-api]]
+                 [org.neo4j/neo4j-graphdb-api "5.3.0"]
+                 [org.neo4j/neo4j-bolt "5.3.0" :exclusions [org.slf4j/slf4j-api]]]
   :pedantic? :warn)
